b00ea73f6994f79e8069c7db65201962e2dc9f09
amrullah-sahmaliyev93 <sahmaliyev.amrullah@bk.ru>
2022-07-06 18:29:26 +0400

    New commit

---
+++ src/az/course/lesson01/Homework1.java
@@ -0,0 +1,7 @@
+package az.course.lesson01;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("Home work 1");
+    }
+}
---
+++ src/az/course/main/Main.java
@@ -0,0 +1,8 @@
+package az.course.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello main");
+        System.out.println("Yes");
+    }
+}
